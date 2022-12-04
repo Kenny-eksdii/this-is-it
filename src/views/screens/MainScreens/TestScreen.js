e7d4c6f8b2abecd5b5b63accd9c8f935daede361
Kenny eksdii <castillo.kennylloyd.bscs2019@gmail.com>
2022-12-04 09:45:05 +0800

    first commit

--- src/views/screens/MainScreens/TestScreen.js
+++ src/views/screens/MainScreens/TestScreen.js
@@ -2,6 +2,13 @@ import { View } from 'react-native'
 import React, { useContext, useState } from 'react'
 import AuthContext from '../../../api/context/auth/AuthContext'
 import { Button, Text } from '../../components/FoodeaComponents'
+// import { SliderBox } from 'react-native-image-slider-box'
+import { ImageSlider } from "react-native-image-slider-banner";
+const TEST_IMAGES = [
+    {img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5a5uCP-n4teeW2SApcIqUrcQApev8ZVCJkA&usqp=CAU'},
+    {img: 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg'},
+    {img: 'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg'}
+];
 
 const TestScreen = () => {
 
@@ -16,6 +23,17 @@ const TestScreen = () => {
             onPress={logout}
             title='logout'
         />
+        <ImageSlider
+            data={TEST_IMAGES}
+            autoPlay={true}
+            // onItemChanged={(item) => console.log("item", item)}
+            closeIconColor="#fff"
+            previewImageStyle	={{
+                borderWidth: 5,
+                borderColor: 'red',
+            }}
+        />
+        {/* <FoodCarousel /> */}
     </View>
   )
 }
